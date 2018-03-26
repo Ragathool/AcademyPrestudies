0c572797585cebf08bef9b9292c394967fff179e
Andreas <Andreasnr13@gmail.com>
2018-03-26 16:11:51 +0200

    Added controller, Viewmodels etc

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcademyPrestudies.Models;
+using AcademyPrestudies.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademyPrestudies.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+        MuninRepository repository;
+
+        public AccountsController(MuninRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [Route ("")]
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateNewUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewUser(CreateNewUserVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Home/Index");
+            }
+        }
+
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# AcademyPrestudies
-SlutprojektVT18
