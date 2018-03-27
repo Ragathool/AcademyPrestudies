a9d47225f3b268a05684b63aa22365397bfe21dc
Cattis <Catrine.matthews@hotmail.com>
2018-03-27 09:30:14 +0200

    compare attribut

--- Models/ViewModels/CreateNewUserVM.cs
+++ Models/ViewModels/CreateNewUserVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,10 @@ namespace AcademyPrestudies.Models.ViewModels
     {
         [Required]
         public string Name { get; set; }
+
+        [NotMapped]
+        [Compare("Name")]
+        public string ConfirmName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
