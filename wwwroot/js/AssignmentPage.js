e60ad522df583236938d830e4ed47b32dbb17fec
Dick Karlsson <karlsson.dick39@gmail.com>
2018-04-02 11:34:33 +0200

    Updated AssignmentPage with togglebuttons, fixed header (also for CourseFrontPage). Added .js-file. Updated StyleSheet.

---
+++ wwwroot/js/AssignmentPage.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#btnsolutionone").click(function () {
+        $("#solutionone").toggle(600);
+    });
+});
+
+$(document).ready(function () {
+    $("#btnsolutiontwo").click(function () {
+        $("#solutiontwo").toggle(600);
+    });
+});
+
+$(document).ready(function () {
+    $("#btnsolutionthree").click(function () {
+        $("#solutionthree").toggle(600);
+    });
+});
