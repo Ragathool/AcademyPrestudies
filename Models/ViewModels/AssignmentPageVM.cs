dc4092447927a55e08a935e4d4299bc11796e896
Andreas Sundin <andreasnr13@gmail.com>
2018-04-15 14:26:43 +0200

    Initial commit on new laptop

---
+++ Models/ViewModels/AssignmentPageVM.cs
@@ -0,0 +1,18 @@
+﻿using AcademyPrestudies.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademyPrestudies.Models.ViewModels
+{
+    public class AssignmentPageVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? CourseId { get; set; }
+        public int? UserId { get; set; }
+        public bool FinishedId { get; set; }
+    }
+}
